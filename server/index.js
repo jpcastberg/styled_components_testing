ef5d3109a89147d30b46de597625be25393345d7
John Castberg <jpcastberg@gmail.com>
2019-05-18 13:54:22 -0700

    Add necessary dependencies

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'client/dist')));
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
