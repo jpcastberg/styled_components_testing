5eda394519e816e7431a77719cd05dfa62b65584
John Castberg <jpcastberg@gmail.com>
2019-05-18 13:55:17 -0700

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client/src/App.jsx'),
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }],
+  },
+  output: {
+    path: path.join(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
