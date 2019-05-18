3cdfb1f56c5a790f3da503f754a7962e01ee562d
John Castberg <jpcastberg@gmail.com>
2019-05-18 14:35:20 -0700

    Add primary and secondary buttons

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,9 +12,21 @@ const Title = styled.h1`
   color: palevioletred;
 `;
 
+const Button = styled.button`
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+
 const App = () => (
   <Wrapper>
     <Title>This is a styled component</Title>
+    <Button primary>Primary Button</Button>
+    <Button>Other Button</Button>
   </Wrapper>
 );
 
